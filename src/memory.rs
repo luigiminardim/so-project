d9fd1dc7c0841a47d263858dd1bc97b41eb50139
luigiminardim <luigiminardim@gmail.com>
2023-07-13 03:54:14 -0300

    fix: :bug: Fix memory specifications.

--- src/memory.rs
+++ src/memory.rs
@@ -1,61 +1,41 @@
-#[derive(Debug, Clone, Copy)]
-pub struct MemoryPartition {
-    pub offset: u32,
-    pub num_blocks: u32,
-}
+use crate::structures::segment_list::{Segment, SegmentList};
 
 pub struct MemoryManager {
-    free_partitions: Vec<MemoryPartition>,
+    real_time_partition: SegmentList,
+    user_partition: SegmentList,
 }
+
+const REAL_TIME_PARTITION_SIZE: usize = 64;
+const USER_PARTITION_SIZE: usize = 960;
 
 impl MemoryManager {
     pub fn new() -> Self {
-        let mut free_partitions = Vec::new();
-        free_partitions.push(MemoryPartition {
+        let real_time_initial_segment = Segment {
             offset: 0,
-            num_blocks: 1024,
-        });
-        Self { free_partitions }
-    }
-
-    pub fn allocate(&mut self, num_blocks: u32) -> Option<MemoryPartition> {
-        for it in self.free_partitions.iter_mut() {
-            if it.num_blocks >= num_blocks {
-                let partition = MemoryPartition {
-                    offset: it.offset,
-                    num_blocks,
-                };
-                it.offset += num_blocks;
-                it.num_blocks -= num_blocks;
-                return Some(partition);
-            }
+            length: REAL_TIME_PARTITION_SIZE,
+        };
+        let user_initial_segment = Segment {
+            offset: REAL_TIME_PARTITION_SIZE,
+            length: USER_PARTITION_SIZE,
+        };
+        Self {
+            real_time_partition: SegmentList::new(vec![real_time_initial_segment]),
+            user_partition: SegmentList::new(vec![user_initial_segment]),
         }
-        None
     }
 
-    pub fn free(&mut self, memory_partition: MemoryPartition) {
-        let mut index = 0;
-        for it in self.free_partitions.iter() {
-            if it.offset > memory_partition.offset {
-                break;
-            }
-            index += 1;
+    pub fn alloc(&mut self, process_priority: u8, size: usize) -> Option<Segment> {
+        match process_priority {
+            0 => self.real_time_partition.alloc(size),
+            _ => self.user_partition.alloc(size),
         }
-        self.free_partitions.insert(index, memory_partition);
-        self.merge_free_partitions();
     }
 
-    fn merge_free_partitions(&mut self) {
-        let mut index = 0;
-        while index < self.free_partitions.len() - 2 {
-            let current = self.free_partitions[index];
-            let next = self.free_partitions[index + 1];
-            if current.offset + current.num_blocks == next.offset {
-                self.free_partitions[index].num_blocks += next.num_blocks;
-                self.free_partitions.remove(index + 1);
-            } else {
-                index += 1;
-            }
+    pub fn free(&mut self, segment: Segment) {
+        if segment.offset < REAL_TIME_PARTITION_SIZE {
+            self.real_time_partition.free(segment);
+        } else {
+            self.user_partition.free(segment);
         }
     }
 }
@@ -64,64 +44,90 @@ impl MemoryManager {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_memory_alloc() {
-        let mut memory_manager = MemoryManager::new();
-        let first_alloc = memory_manager.allocate(8).unwrap();
-        let second_alloc = memory_manager.allocate(16).unwrap();
-        assert_eq!(first_alloc.offset, 0);
-        assert_eq!(first_alloc.num_blocks, 8);
-        assert_eq!(second_alloc.offset, 8);
-        assert_eq!(second_alloc.num_blocks, 16);
-    }
-
-    mod free {
+    mod alloc {
         use super::*;
 
         #[test]
-        fn test_free() {
+        fn real_time_process_allocs_from_first_partition() {
             let mut memory_manager = MemoryManager::new();
-            let first_partition = memory_manager.allocate(8).unwrap();
-            let _second_partition = memory_manager.allocate(16).unwrap();
-            memory_manager.free(first_partition);
-            let second_alloc_of_first_partition = memory_manager.allocate(8).unwrap();
-            assert_eq!(second_alloc_of_first_partition.offset, 0);
-            assert_eq!(second_alloc_of_first_partition.num_blocks, 8);
+            let alloc_segment = memory_manager.alloc(0, 10);
+            assert_eq!(
+                alloc_segment,
+                Some(Segment {
+                    offset: 0,
+                    length: 10
+                })
+            );
         }
 
         #[test]
-        fn test_merge_prev() {
+        fn user_process_allocs_from_second_partition() {
             let mut memory_manager = MemoryManager::new();
-            let first_partition = memory_manager.allocate(8).unwrap();
-            let second_partition = memory_manager.allocate(16).unwrap();
-            let _third_partition = memory_manager.allocate(8).unwrap();
-            memory_manager.free(first_partition);
-            memory_manager.free(second_partition);
-            let alloc = memory_manager.allocate(24).unwrap();
-            assert_eq!(alloc.offset, 0);
-            assert_eq!(alloc.num_blocks, 24);
+            let alloc_segment = memory_manager.alloc(1, 10);
+            assert_eq!(
+                alloc_segment,
+                Some(Segment {
+                    offset: 64,
+                    length: 10
+                })
+            );
         }
 
         #[test]
-        fn test_merge_next() {
+        fn real_time_process_dont_access_user_partition() {
             let mut memory_manager = MemoryManager::new();
-            let first_partition = memory_manager.allocate(8).unwrap();
-            memory_manager.free(first_partition);
-            let alloc = memory_manager.allocate(8).unwrap();
-            assert_eq!(alloc.offset, 0);
-            assert_eq!(alloc.num_blocks, 8);
+            let alloc_segment = memory_manager.alloc(0, 65);
+            assert_eq!(alloc_segment, None);
+        }
+    }
+
+    mod free {
+        use crate::memory;
+
+        use super::*;
+
+        #[test]
+        fn real_time_partition_segments_can_be_reused() {
+            let mut memory_manager = MemoryManager::new();
+            let alloc_segment = memory_manager.alloc(0, 10);
+            assert_eq!(
+                alloc_segment,
+                Some(Segment {
+                    offset: 0,
+                    length: 10
+                })
+            );
+            memory_manager.free(alloc_segment.unwrap());
+            let alloc_segment = memory_manager.alloc(0, 10);
+            assert_eq!(
+                alloc_segment,
+                Some(Segment {
+                    offset: 0,
+                    length: 10
+                })
+            );
         }
 
         #[test]
-        fn test_merge_prev_and_next() {
+        fn user_partition_segments_can_be_reused() {
             let mut memory_manager = MemoryManager::new();
-            let first_partition = memory_manager.allocate(8).unwrap();
-            let second_partition = memory_manager.allocate(16).unwrap();
-            memory_manager.free(first_partition);
-            memory_manager.free(second_partition);
-            let alloc = memory_manager.allocate(24).unwrap();
-            assert_eq!(alloc.offset, 0);
-            assert_eq!(alloc.num_blocks, 24);
+            let alloc_segment = memory_manager.alloc(1, 10);
+            assert_eq!(
+                alloc_segment,
+                Some(Segment {
+                    offset: 64,
+                    length: 10
+                })
+            );
+            memory_manager.free(alloc_segment.unwrap());
+            let alloc_segment = memory_manager.alloc(1, 10);
+            assert_eq!(
+                alloc_segment,
+                Some(Segment {
+                    offset: 64,
+                    length: 10
+                })
+            );
         }
     }
 }
