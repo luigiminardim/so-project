a5277f7b14ade2ad2ee5123d5d496ed30d322196
luigiminardim <luigiminardim@gmail.com>
2023-07-12 22:12:43 -0300

    feat: :sparkles: queues module

--- src/queues.rs
+++ src/queues.rs
@@ -3,12 +3,12 @@ use std::collections::VecDeque;
 use crate::process::Process;
 
 pub struct ExecutionContext {
-    pub process: Process,
+    process: Process,
     start_time: usize,
 }
 
 pub struct ProcessManager {
-    pub execution: Option<ExecutionContext>,
+    execution: Option<ExecutionContext>,
     queues: Vec<VecDeque<Process>>,
 }
 
@@ -18,6 +18,12 @@ impl ProcessManager {
             execution: None,
             queues: (0..3).map(|_| VecDeque::new()).collect(),
         }
+    }
+
+    pub fn get_current_process(&mut self) -> Option<&mut Process> {
+        self.execution
+            .as_mut()
+            .map(|execution_context| &mut execution_context.process)
     }
 
     fn fill_executing_context(&mut self, timestamp: usize) {
@@ -43,6 +49,29 @@ impl ProcessManager {
         self.fill_executing_context(timestamp);
         Some(process)
     }
+
+    pub fn on_tick(&mut self, timestamp: usize) {
+        const USER_PROCESS_QUANTUM: usize = 1;
+        let should_change_context = match self.execution.as_ref() {
+            None => true,
+            Some(execution_context) => match execution_context.process.software_context.priority {
+                0 => false,
+                _ => timestamp - execution_context.start_time >= USER_PROCESS_QUANTUM,
+            },
+        };
+        if should_change_context {
+            match self.execution.take() {
+                None => {}
+                Some(execution_context) => {
+                    let mut process = execution_context.process;
+                    process.software_context.priority =
+                        std::cmp::min(process.software_context.priority + 1, 3);
+                    self.add_process(process, timestamp);
+                    self.fill_executing_context(timestamp);
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -57,8 +86,68 @@ mod tests {
     fn test_add_process() {
         let mut process_manager = ProcessManager::new();
         let process = create_process_mock(0);
-        assert!(process_manager.execution.is_none());
+        assert!(process_manager.get_current_process().is_none());
         process_manager.add_process(process, 0);
-        assert!(process_manager.execution.is_some());
+        assert!(process_manager.get_current_process().is_some());
+    }
+
+    #[test]
+    fn test_block_current_process() {
+        let mut process_manager = ProcessManager::new();
+        let process0 = create_process_mock(0);
+        process_manager.add_process(process0, 0);
+        let process1 = create_process_mock(1);
+        process_manager.add_process(process1, 0);
+        assert!(process_manager.get_current_process().is_some());
+        process_manager.block_current_process(0);
+        assert!(process_manager.get_current_process().is_some());
+        process_manager.block_current_process(0);
+        assert!(process_manager.get_current_process().is_none());
+    }
+
+    #[test]
+    fn test_on_tick() {
+        let mut process_manager = ProcessManager::new();
+        let real_time_process = create_process_mock(0);
+        let user_process = create_process_mock(1);
+        process_manager.add_process(real_time_process, 0);
+        process_manager.add_process(user_process, 0);
+        assert!(
+            process_manager
+                .get_current_process()
+                .unwrap()
+                .software_context
+                .priority
+                == 0
+        );
+        process_manager.on_tick(1);
+        assert!(
+            process_manager
+                .get_current_process()
+                .unwrap()
+                .software_context
+                .priority
+                == 0
+        );
+
+        let real_time_process = process_manager.block_current_process(1).unwrap();
+        process_manager.add_process(real_time_process, 1);
+        assert_eq!(
+            process_manager
+                .get_current_process()
+                .unwrap()
+                .software_context
+                .priority,
+            1
+        );
+        process_manager.on_tick(2);
+        assert_eq!(
+            process_manager
+                .get_current_process()
+                .unwrap()
+                .software_context
+                .priority,
+            0
+        );
     }
 }
